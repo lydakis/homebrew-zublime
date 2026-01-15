fab23f50d01b503f328b81b50a54a4682602d511
George Lydakis <george@lydakis.me>
2026-01-15 10:10:21 -0500

    Point cask to v0.1.0 arm64 DMG

--- Casks/zublime.rb
+++ Casks/zublime.rb
@@ -1,15 +1,10 @@
 cask "zublime" do
-  version "0.1.1"
+  version "0.1.0"
 
-  on_arm do
-    sha256 "PLACEHOLDER_SHA256_ARM64"
-    url "https://github.com/lydakis/zublime/releases/download/v#{version}/Zublime-aarch64.dmg"
-  end
+  depends_on arch: :arm64
 
-  on_intel do
-    sha256 "PLACEHOLDER_SHA256_X86_64"
-    url "https://github.com/lydakis/zublime/releases/download/v#{version}/Zublime-x86_64.dmg"
-  end
+  sha256 "5573df9d63ab519ee35bf0e9bfc6eabf7168552995519355dbcc1ec50790fe37"
+  url "https://github.com/lydakis/zublime/releases/download/v#{version}/Zublime-aarch64.dmg"
 
   name "Zublime"
   desc "A fork of Zed editor"
@@ -20,13 +15,13 @@ cask "zublime" do
     strategy :github_latest
   end
 
-  app "Zed.app", target: "Zublime.app"
+  app "Zublime.app"
 
   zap trash: [
     "~/Library/Application Support/Zublime",
     "~/Library/Caches/Zublime",
     "~/Library/Logs/Zublime",
-    "~/Library/Preferences/dev.zublime.Zublime.plist",
-    "~/Library/Saved Application State/dev.zublime.Zublime.savedState",
+    "~/Library/Preferences/ooo.engineered.Zublime.plist",
+    "~/Library/Saved Application State/ooo.engineered.Zublime.savedState",
   ]
 end
